4b6ad25057552cd1351baaaa24a839f22225905f
Peter Schwarz <peter.schwarz@bicycle.io>
2014-02-09 19:13:13 -0600

    Updated for release

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clj-serial "1.0.0-SNAPSHOT"
-  :description "Simple serial port comms library. Wraps PureJavaComm."
+(defproject clj-serial "1.0.0"
+  :description "Simple serial port library. Wraps PureJavaComm."
   :url "https://github.com/peterschwarz/clj-serial"
   :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo}
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.sparetimelabs/purejavacomm "0.0.21"]]
-  :repositories [["javacomm" "http://www.sparetimelabs.com/maven2"]])
+  :repositories [["javacomm" "http://www.sparetimelabs.com/maven2"]]
+  :scm {:name "git"
+        :url "https://github.com/peterschwarz/clj-serial"})

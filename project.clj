c8c4f620a489c6488c261c154c75bc32a003299e
Peter Schwarz <peter.schwarz@bicycle.io>
2014-04-07 14:46:28 -0500

    ready for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-serial "2.0.1"
+(defproject clj-serial "2.0.2-SNAPSHOT"
   :description "Simple serial port library. Wraps PureJavaComm."
   :url "https://github.com/peterschwarz/clj-serial"
   :license {:name "Eclipse Public License - v 1.0"

27babd4a81fc0adf537659444c3c0b99b8f62f6c
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-03-27 14:06:36 -0700

    Add get-port-names helper to util
    It's frequently useful to get the set of the available port names,
    and then be able to do work over it. This just exposes a helper
    so that the caller doesn't need to know what a CommPortIdentifier
    is and how to get it's name.

--- src/serial/util.clj
+++ src/serial/util.clj
@@ -1,8 +1,14 @@
 (ns serial.util
-  (:require [serial.core :refer [port-identifiers]]))
+  (:require [serial.core :refer [port-identifiers]])
+  (:import [purejavacomm CommPortIdentifier]))
+
+(defn get-port-names
+  "Gets a set of the currently avilable port names"
+  []
+  (set (map #(.getName ^CommPortIdentifier %) (port-identifiers))))
 
 (defn list-ports
   "Print out the available ports.
    The names printed may be passed to `serial.core/open` as printed."
   []
-  (doall (map #(println (.getName %)) (port-identifiers))))
+  (doall (map println (get-port-names))))

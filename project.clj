edc59f5edd116004088a850248d058abed967210
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-11-14 11:14:35 -0500

    Changing purejavacomm source to avoid insecure http repo

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.sparetimelabs/purejavacomm "1.0.1"]]
-  :repositories [["javacomm" "http://www.sparetimelabs.com/maven2"]]
+                 [com.github.purejavacomm/purejavacomm "1.0.1.RELEASE"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :scm {:name "git"

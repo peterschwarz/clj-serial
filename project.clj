281490091c9e9f576abb76b13c13d7a2a9d6463f
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-12-20 12:57:30 -0500

    Update purejavacomm to 1.0.2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.github.purejavacomm/purejavacomm "1.0.1.RELEASE"]]
+                 [com.github.purejavacomm/purejavacomm "1.0.2.RELEASE"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :scm {:name "git"

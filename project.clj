e3226f1d9794578a75a6dfe9e2f3a65e1579112d
Peter Schwarz <peter.schwarz@bicycle.io>
2018-08-18 11:55:28 -0500

    Version bump to 2.0.6-SNAPSHOT
    Preperatory version bump for new development
    
    Signed-off-by: Peter Schwarz <peter.schwarz@bicycle.io>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-serial "2.0.5"
+(defproject clj-serial "2.0.6-SNAPSHOT"
   :description "Simple serial port library. Wraps PureJavaComm."
   :url "https://github.com/peterschwarz/clj-serial"
   :license {:name "Eclipse Public License - v 1.0"
